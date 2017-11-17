3c393af74f11b6ee3168a18ad891bda18b547faf
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-17 23:49:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
  return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  list_of_names.collect do |attendee|
-    badge_maker(attendee)
+def batch_badge_creator(names)
+  list_of_names.collect do |name|
+    badge_maker(name)
   end
 end
 
