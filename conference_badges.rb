8ff1cc8e1da494e6eba3fa2953897842493cbf53
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-17 23:47:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,14 @@ def badge_maker(name)
  return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(list_of_names)
+def batch_badge_creator(names)
   list_of_names.collect do |name|
     badge_maker(name)
   end
 end
 
 def assign_rooms(list_of_names)
-  return room_assignments = list_of_names.each_with_index do |name, room|
+  list_of_names.each_with_index do |name, room|
     "Hello #{name}! You'll be assigned to room #{room}!"
   end
 end
